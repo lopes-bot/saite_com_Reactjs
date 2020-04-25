26c4a20d69c1d869fde2a3bc2de9444766be2a76
unknown <andrelopessfla@gmail.com>
2020-04-25 03:46:32 -0300

    adicionado os estados

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./App.css";
-
+/*
 function Helloword(props) {
   console.log("tag", props);
   return (
@@ -10,8 +10,33 @@ function Helloword(props) {
       {props.mytext}
     </div>
   );
+}*/
+class Helloword extends React.Component {
+  state = {
+    show: true,
+  };
+  togleshow() {
+    this.setState({ show: !this.state.show });
+  }
+  render() {
+    if (this.state.show) {
+      return (
+        <div id="hello">
+          <h3>{this.props.subtitle}</h3>
+          <p>{this.props.mytext}</p>
+          <button onClick={() => this.togleshow()}>togle show</button>
+        </div>
+      );
+    } else {
+      return (
+        <h2>
+          nao tem elementos
+          <button onClick={() => this.togleshow()}>togle show</button>
+        </h2>
+      );
+    }
+  }
 }
-
 function App() {
   return (
     <div>
