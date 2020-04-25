6e307bda2c029d389ffbf5e36ca05d651b556d5e
unknown <andrelopessfla@gmail.com>
2020-04-25 01:38:23 -0300

    criando componetes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import React from "react";
-
 import "./App.css";
 
+function Helloword() {
+  return <div id="hello">Hello Word!</div>;
+}
+
 function App() {
-  return <div>Hello Word!</div>;
+  return (
+    <div>
+      <p>
+        esse e o meu componete:
+        <Helloword />
+      </p>
+    </div>
+  );
 }
 
 export default App;
