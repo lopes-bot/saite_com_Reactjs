391ebbc853c7866239e87d3650a588d130189c48
unknown <andrelopessfla@gmail.com>
2020-04-25 19:19:52 -0300

    criado mutiplos componetes

--- src/App.js
+++ src/App.js
@@ -1,53 +1,16 @@
-import React from "react";
+import React, { Component } from "react";
 import "./App.css";
-/*
-function Helloword(props) {
-  console.log("tag", props);
-  return (
-    <div id="hello">
-      {props.subtitle}
-      <br></br>
-      {props.mytext}
-    </div>
-  );
-}*/
-class Helloword extends React.Component {
+import tasks from "./sample/tasks.json";
+
+import Tasks from "./components/Tasks";
+
+class App extends Component {
   state = {
-    show: true,
+    tasks: tasks,
   };
-  togleshow() {
-    this.setState({ show: !this.state.show });
-  }
   render() {
-    if (this.state.show) {
-      return (
-        <div id="hello">
-          <h3>{this.props.subtitle}</h3>
-          <p>{this.props.mytext}</p>
-          <button onClick={() => this.togleshow()}>togle show</button>
-        </div>
-      );
-    } else {
-      return (
-        <h2>
-          nao tem elementos
-          <button onClick={() => this.togleshow()}>togle show</button>
-        </h2>
-      );
-    }
+    return <Tasks tasks={this.state.tasks} />;
   }
-}
-function App() {
-  return (
-    <div>
-      <p>
-        esse e o meu componete:
-        <Helloword mytext="ola novamente" subtitle="eu sou o cara" />
-        <Helloword mytext="nossa" subtitle="testando" />
-        <Helloword mytext="oi eu sou o goku" subtitle="componete" />
-      </p>
-    </div>
-  );
 }
 
 export default App;
