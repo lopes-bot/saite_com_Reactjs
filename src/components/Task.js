e7094078ba7927bb0e3153309be7e9d89bdfddfe
unknown <andrelopessfla@gmail.com>
2020-04-26 00:11:46 -0300

    criado o style

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,16 +1,36 @@
 import React, { Component } from "react";
+import "./Task.css";
 
 class Task extends Component {
+  styleComplite() {
+    return {
+      fontSize: "20px",
+      color: this.props.task.done ? "gray" : "black",
+      textDecoration: this.props.task.done ? "line-through  " : "none",
+    };
+  }
   render() {
     const { task } = this.props;
     return (
       <div>
-        {task.title}-{task.description}-{task.done}-{task.id}
-        <input type="checkbox" />
-        <button>x</button>
+        <p style={this.styleComplite()}>
+          {task.title}-{task.description}-{task.done}-{task.id}
+          <input type="checkbox" />
+          <button style={btnDelete}>x</button>
+        </p>
       </div>
     );
   }
 }
+
+const btnDelete = {
+  fontSize: "18px",
+  background: "#ea2017",
+  color: "#fff",
+  border: "none",
+  padding: "10px 15px",
+  borderRadius: "50%",
+  cursor: "pointer",
+};
 
 export default Task;
